07a9eada5c0706dab60988b94154d891643bfe36
sxrgio12 <sergioherreroballinote@gmail.com>
2025-05-27 17:39:46 +0200

    Update js.js

--- js.js
+++ js.js
@@ -31,12 +31,6 @@ function normalizar(texto) {
  * @param {string} respuestaCorrecta - Respuesta correcta de la pregunta.
  * @returns {boolean} - `true` si la respuesta es correcta, `false` en caso contrario.
  */
-//Añadido abajo
-if (!preguntaActual || !preguntaActual.opciones) {
-    alert("Error interna: Pregunta no válida.");
-    return;
-}
-//Añadido arriba
 function esRespuestaCorrecta(respuestaUsuario, pregunta) {
     const opcionCorrecta = pregunta.opciones[pregunta.respuesta_correcta_index];
     return respuestaUsuario === opcionCorrecta;
